839e43c309d87644e19bef6678575e39ec9547cf
Tobias Riesbeck <tobias.riesbeck@letsdev.de>
2022-07-12 11:03:58 +0200

    Project setup.

---
+++ app/src/main/java/de/netid/mobile/sdk/MainActivity.kt
@@ -0,0 +1,12 @@
+package de.netid.mobile.sdk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
