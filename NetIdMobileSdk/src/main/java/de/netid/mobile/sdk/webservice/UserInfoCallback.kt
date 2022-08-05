da2176c501d7eb037ef6e4686a8705aebe2abb01
Tobias Riesbeck <tobias.riesbeck@letsdev.de>
2022-08-05 17:00:41 +0200

    Added documentation for web service classes.

--- NetIdMobileSdk/src/main/java/de/netid/mobile/sdk/webservice/UserInfoCallback.kt
+++ NetIdMobileSdk/src/main/java/de/netid/mobile/sdk/webservice/UserInfoCallback.kt
@@ -3,8 +3,22 @@ package de.netid.mobile.sdk.webservice
 import de.netid.mobile.sdk.api.NetIdError
 import de.netid.mobile.sdk.model.UserInfo
 
+/**
+ * Defines functions which are called after a user info request is complete.
+ */
 interface UserInfoCallback {
+
+    /**
+     * Is called, when a user info request was successful. Provides the fetched information.
+     *
+     * @param userInfo a [UserInfo] instance representing the fetched information
+     */
     fun onUserInfoFetched(userInfo: UserInfo)
 
+    /**
+     * Is called, when a user info request failed. Provides related error information.
+     *
+     * @param error a [NetIdError] instance describing the occurred error
+     */
     fun onUserInfoFetchFailed(error: NetIdError)
 }
--- NetIdMobileSdk/src/main/java/de/netid/mobile/sdk/webservice/WebserviceApi.kt
+++ NetIdMobileSdk/src/main/java/de/netid/mobile/sdk/webservice/WebserviceApi.kt
@@ -1,5 +1,6 @@
 package de.netid.mobile.sdk.webservice
 
+import android.os.Handler
 import android.os.Looper
 import de.netid.mobile.sdk.api.NetIdError
 import de.netid.mobile.sdk.api.NetIdErrorCode
@@ -8,14 +9,31 @@ import de.netid.mobile.sdk.constants.WebserviceConstants
 import de.netid.mobile.sdk.model.UserInfo
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-import okhttp3.*
-import android.os.Handler
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import java.io.IOException
 
+/**
+ * Provides functions to perform web requests.
+ */
 object WebserviceApi {
 
+    /**
+     * A [OkHttpClient] instance to enqueue web calls
+     */
     private val client = OkHttpClient()
 
+    /**
+     * Performs a request to fetch information related to an authorized user.
+     * The result of the request is provided via the given [UserInfoCallback] instance.
+     *
+     * @param accessToken a currently valid token to authenticate and identify a specific user
+     * @param host the URL string of the host processing the request
+     * @param userInfoCallback a [UserInfoCallback] instance receiving callbacks when the request is complete
+     */
     fun performUserInfoRequest(
         accessToken: String,
         host: String,
