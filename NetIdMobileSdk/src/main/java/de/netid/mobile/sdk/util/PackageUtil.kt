ae8575947e411e5bac1b5b738092cf146c199a96
Tobbias Bachmor <tobias.bachmor@letsdev.de>
2022-09-28 18:49:21 +0200

    fixed finding of activities

--- NetIdMobileSdk/src/main/java/de/netid/mobile/sdk/util/PackageUtil.kt
+++ NetIdMobileSdk/src/main/java/de/netid/mobile/sdk/util/PackageUtil.kt
@@ -54,12 +54,12 @@ class PackageUtil {
          */
         private fun isPackageInstalled(packageName: String, activityName: String, packageManager: PackageManager): Boolean {
             return try {
-                val info = packageManager.getPackageInfo(packageName, 0)
-                // Todo: Filter for specific activity
+                val info = packageManager.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES)
+                // Filter for specific activity
                 if (info.activities != null) {
                     info.activities.forEach {
-                        if (it.packageName.equals(activityName)) {
-                            true
+                        if (it.name.equals(activityName)) {
+                                true
                         }
                     }
                 }
