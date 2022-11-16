046012bb64b62a3832c585877c25d446afa7917e
Achim <achim.schlosser@enid.eu>
2022-11-16 03:17:15 +0100

    Default intent processing to avoid activity mixup

--- NetIdMobileSdk/src/main/java/de/netid/mobile/sdk/appauth/AppAuthManagerImpl.kt
+++ NetIdMobileSdk/src/main/java/de/netid/mobile/sdk/appauth/AppAuthManagerImpl.kt
@@ -39,7 +39,6 @@ class AppAuthManagerImpl : AppAuthManager {
     private var authorizationServiceConfiguration: AuthorizationServiceConfiguration? = null
     private var authState: AuthState? = null
     private var authService: AuthorizationService? = null
-    private var authRequest: AuthorizationRequest? = null
 
     override fun getAccessToken(): String? {
         return authState?.accessToken
@@ -106,10 +105,9 @@ class AppAuthManagerImpl : AppAuthManager {
                     Uri.parse(redirectUri)
                 ).setScopes(scopes
                 ).setClaims(claimsJSON)
-            authRequest = authRequestBuilder.build()
-
+            val authRequest = authRequestBuilder.build()
             authService = AuthorizationService(activity)
-            return authService?.getAuthorizationRequestIntent(authRequest!!)
+            return authService?.getAuthorizationRequestIntent(authRequest)
         } ?: run {
             Log.e(javaClass.simpleName, "No authorization service configuration available")
             return null
@@ -117,14 +115,16 @@ class AppAuthManagerImpl : AppAuthManager {
     }
 
     override fun processAuthorizationIntent(data: Intent) {
-        val authorizationResponse = AuthorizationResponse.Builder(authRequest!!).fromUri(data.data!!).build()
-        var authorizationException = AuthorizationException.fromIntent(data)
+        val authorizationResponse = AuthorizationResponse.fromIntent(data)
+        val authorizationException = AuthorizationException.fromIntent(data)
+
+        authState?.update(authorizationResponse,authorizationException)
 
         authorizationException?.let {
             val netIdError = createNetIdErrorForAuthorizationException(it)
             listener?.onAuthorizationFailed(netIdError)
         } ?: run {
-            authorizationResponse.let {
+            authorizationResponse?.let {
                 processTokenExchange(it)
             }
         }
